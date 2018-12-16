10cdb8f7249a33d5d159d59e73aee860583db758
mariomui <yi.kan.mui@gmail.com>
2018-12-15 16:52:48 -0800

    Created Repos

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const path = require('path');
+
 const app = express();
 
 const getReposByUserName = require('../helpers/github.js').getReposByUsername;
 
-const save = require('../database').save
-const path = require('path');
+const { save } = require('../database');
 
 var defaultRoute = path.join(__dirname, '../client/dist');
 
@@ -21,6 +22,7 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  console.log(typeof req.body, 'what is reqbody afer json');
   var clientSearchTerm = req.body.term;
   getReposByUserName(clientSearchTerm, (error, data) => {
     //do the db.query but for whatever.
@@ -28,11 +30,25 @@ app.post('/repos', function (req, res) {
       console.log(error);
       return;
     }
-    var curatedData = {
-      pushed_at: data.pushed_at,
-      html_url: data.html_url,
-    };
-    console.dir(curatedData, 'this is my data');
+    // [{},{},{}]
+    // console.dir(JSON.parse(data), 'this is my data');
+    var curatedRepos = Array.from(JSON.parse(data)).map((repos) => {
+      return (
+        {
+          repo_id: repos.id,
+          repo_owner: repos.full_name,
+          owner: {
+            owner_id: repos.owner.id,
+            owner_name: repos.owner.login,
+          },
+          pushed_at: repos.pushed_at,
+          url: repos.html_url,
+        }
+      );
+    });
+    console.log(curatedRepos);
+
+    // console.log(curatedRepos, 'test');
     // save(data, (err, saveReceipt) => {
     //   if (err) {
     //     console.log(err);
